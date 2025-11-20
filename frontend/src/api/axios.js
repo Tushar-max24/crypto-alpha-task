37ab8cc8c2e1492b10f11a8772f87ecab159b71f
Tushar-max24 <tusharjangid98870@gmail.com>
2025-11-20 09:31:12 +0530

    Updated env and axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://crypto-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://crypto-alpha-task.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
